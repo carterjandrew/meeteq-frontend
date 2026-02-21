977955b1c4cc6745e09ea41d6f90cfcaadfd138b
Carter <carterinmotion@gmail.com>
2026-02-20 21:04:51 -0400

    Login flow working

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import { Button } from "~/components/ui/button";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +10,7 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+		<Button>Hello</Button>
+	)
 }
